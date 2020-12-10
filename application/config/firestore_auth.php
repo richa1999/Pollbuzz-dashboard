8992ab7e2750d0b051b5ffd613e6af169e48307b
richa1999 <richa.triceria@gmail.com>
2020-12-10 13:52:44 +0530

    firebaseid

--- application/config/firestore_auth.php
+++ application/config/firestore_auth.php
@@ -12,6 +12,6 @@ $config['project_ID'] = '';
 | keyfilepath
 |--------------------------------------------------------------------------
 */
-$config['keyFilePath'] = APPPATH.'\config\glass-pen-271820-firebase-adminsdk-u3mnl-c8f6b5b1b6.json';
+$config['keyFilePath'] = ;
 
 
